091284dc648bd86a952e94dc6961e62fd1ea3cf5
Joeri Smissaert <smissaertj@gmail.com>
2022-06-25 13:29:33 +0400

    Prevent signup if username exists

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 /*
-User Profiles Array:
-Stores an array of user object which contain
-Username, Password, Quiz attempts, Collected Stars and Ranking
-*/
-let userProfiles = [];
-
-/*
 User Sign up
 */
 const createUserProfile = (username, passwordHash) => {
-  let user = {
-    'username': username,
-    'password': passwordHash
-  }
+  // retrieve the existing users or create an empty array
+  let users = JSON.parse(localStorage.getItem('userProfiles') || '[]');
+  console.log(users);
 
-  userProfiles.push(user);
-  localStorage.setItem('userProfiles', JSON.stringify(user));
+  let usernameExists = users.some(user => user.username === username);
+
+  if (usernameExists) {
+    return false;
+  } else {
+    user = {
+      'username': username,
+      'password': passwordHash
+    }
+    users.push(user);
+    localStorage.setItem('userProfiles', JSON.stringify(users));
+    return true;
+  }
 }
 
 
@@ -32,29 +35,32 @@ btnSignUp.addEventListener('click', function(ev){
   let userName =document.getElementById('signupUsername').value;
   let passwordHash = btoa(document.getElementById('signupPassword').value);
 
-  createUserProfile(userName, passwordHash);
-
-  // Consider the user to be logged in.
-  localStorage.setItem('currentLoggedInUser', userName);
-
-  // Update the modal text
   let modalHeader = document.querySelectorAll('.modal-header')[0];
-  modalHeader.firstElementChild.textContent = "Account Created!";
-
   let signupForm = document.getElementById('signupForm');
   signupForm.parentElement.removeChild(signupForm);
-
   let modalBody = document.querySelectorAll('.modal-body')[0];
   let p = document.createElement('p');
-  p.innerHTML = '<small>You may close this window and login.</small>';
-  modalBody.appendChild(p);
-
-  // Close the modal on signupBtnClose click event
   let btnClose = document.getElementById('signupBtnClose');
-  btnClose.addEventListener('click', function(ev){
+
+  if (!createUserProfile(userName, passwordHash)){
+    modalHeader.firstElementChild.textContent = "Username exists.";
+    p.innerHTML = '<small>Please choose a different username.</small>';
+    modalBody.appendChild(p);
+    btnClose.addEventListener('click', function(ev){
     ev.preventDefault();
     closeModal('getStarted');
+    location.reload();
   })
+  } else {
+    modalHeader.firstElementChild.textContent = "Account Created!";
+    p.innerHTML = '<small>You may close this window and login.</small>';
+    modalBody.appendChild(p);
+    btnClose.addEventListener('click', function(ev){
+    ev.preventDefault();
+    closeModal('getStarted');
+    })
+  }
+
 })
 
 
